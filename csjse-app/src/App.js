892d85a227bdd7d84bc0fca2e6fc729412bc9d11
Josh <calcyrer@gmail.com>
2023-11-06 07:58:15 -0600

    Login button on home page now takes you to login page

--- csjse-app/src/App.js
+++ csjse-app/src/App.js
@@ -1,9 +1,6 @@
 //import logo from './logo.svg';
 import './App.css';
 import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Profile from "./pages/Profile";
-import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
--- csjse-app/src/pages/Home.js
+++ csjse-app/src/pages/Home.js
@@ -1,19 +1,28 @@
 import React from "react";
 import Button from "../components/Button";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import Login from "./Login";
 
 function Home() {
   return (
-    <div className="m-5">
-      <div className="flex flex-row gap-5">
-        <div className="basis-3/5">
-          <h1 className="text-3xl font-medium">Christian Schools Job Board</h1>
-        </div>
-        <div className="basis-2/5 flex flex-row gap-5 items-center justify-end">
-          <Button bg="black" btnText="Create Account" textColor="text-white" />
-          <Button bg="black" btnText="Login" textColor="text-white" />
+    <Router>
+      <div className="m-5">
+        <div className="flex flex-row gap-5">
+          <div className="basis-3/5">
+            <h1 className="text-3xl font-medium">Christian Schools Job Board</h1>
+          </div>
+          <div className="basis-2/5 flex flex-row gap-5 items-center justify-end">
+            <Button bg="black" btnText="Create Account" textColor="text-white" />
+            <Link to="/Login">Login</Link>
+          </div>
         </div>
       </div>
-    </div>
+      <Routes>
+        <Route path="/Login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+    
   );
 }
 

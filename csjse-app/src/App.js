f5175312d3fc7f6e57e5f3d95ff905fec40cf90c
Josh <calcyrer@gmail.com>
2023-11-06 06:43:41 -0600

    Set up basic login page and cleaned app.css

--- csjse-app/src/App.js
+++ csjse-app/src/App.js
@@ -1,6 +1,9 @@
 //import logo from './logo.svg';
-//import './App.css';
+import './App.css';
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (

290c7de5bac2867d13914f9afe5477e4a6a48ed2
Mattj402 <m.mcglaughlin6@gmail.com>
2023-11-13 20:43:11 -0600

    Port change

--- csjse-app/server/server.js
+++ csjse-app/server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mysql = require('mysql2')
 const bodyParser = require('body-parser')
 const app = express()
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 5000
 
 app.use(bodyParser.json()) // Use JSON parser for incoming requests
 

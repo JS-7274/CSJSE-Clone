1800c14a5e397e4f07151f042eeccab3e5411938
Josh-667 <77750100+Josh-667@users.noreply.github.com>
2023-12-04 15:56:11 -0600

    Better detailed comments

--- csjse-app/client/src/pages/SchoolLogin.js
+++ csjse-app/client/src/pages/SchoolLogin.js
@@ -35,14 +35,21 @@ export default function SchoolLogin() {
 	}
 
 	return (
+		// Puts everything in a container to change bg color
 		<div className="backgroundColor">
+			{/*Another container to change style*/}
 			<div className="login-container">
+				{/*Creates a form using the login-form styling and the handleSubmit functoin when the form is submitted*/}
 				<form className="login-form" onSubmit={handleSubmit}>
+					{/*Creates a header with the text "Login"*/}
 					<h2>Login</h2>
+					{/*Creates a container for a specific item in the form*/}
 					<div className="form-group">
+						{/*Names the item "Email" with the given styling*/}
 						<label className="label" htmlFor="email">
 							Email
 						</label>
+						{/*Creates an input field that will take in text of type email and give it the id email*/}
 						<input
 							className="input-field"
 							value={email}
@@ -54,9 +61,11 @@ export default function SchoolLogin() {
 						/>
 					</div>
 					<div className="form-group">
+						{/*Creates a label with the text "Password"*/}
 						<label className="label" htmlFor="password">
 							Password
 						</label>
+						{/*Creates an input that will take in a text of type password and give it the id password*/}
 						<input
 							className="input-field"
 							value={pass}
@@ -67,19 +76,14 @@ export default function SchoolLogin() {
 							name="password"
 						/>
 					</div>
+					{/*Creates a button that will serve as the sign to submit the fields given using the styling from "button" with the text "Log In" displayed*/}
 					<button type="submit" className="button">
 						Log In
 					</button>
+					{/*Creates a button that will use the handleCreateAccount function to send someone to the create account page for their specified account type with the text "Create Account" displayed*/}
 					<button type="button" className="button" onClick={handleCreateAccount}>Create Account</button>
 				</form>
 			</div>
 		</div>
 	);
 }
-//<div className = "App"> gives the div the name App which lets it reference a style in app.css
-//<div className = "auth...container"> gives the div that name to apply stylings from app.css
-//<h2>Login</h2> header for the login form
-//<form className='login-form'...> start of a form element  and takes in the handle submit function from above
-//<label htmlfor-"email"> marks a html element taking a form input, 'thmlfor="email"' gives it the id 'email'
-//<input value={email}... creates an input element that will take in an email (variable made earlier) then calls the setEmail function to what is input
-//<button type="submit"... creates a button html element, type="submit" makes it trigger a submit of the form on click, the >Log In< gives the button the text "Log In"

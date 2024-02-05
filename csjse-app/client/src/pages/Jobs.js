ee34a43a6856948e14191b4a38be3747a123b9ac
autumn-mckee <autumnpmckee@gmail.com>
2024-02-05 09:16:09 -0600

    page skeleton created

--- csjse-app/client/src/pages/Jobs.js
+++ csjse-app/client/src/pages/Jobs.js
@@ -1,11 +1,26 @@
 import React from "react";
 import { TeacherStaffHeader } from "../components/Headers";
+import "../styles/Jobs.css";
 
 function Jobs() {
 	return (
 		<div>
 			<TeacherStaffHeader></TeacherStaffHeader>
-			<h1>Jobs</h1>
+			<div className="search-bar">
+				Search
+				{/*add hamburger icon and searcch icon*/}
+			</div>
+			<div className="info-display">
+				<div className="job-navigation">
+					{/*add navigation buttons for jobs*/}
+					<button>Teacher</button>
+				</div>
+				<div className="job-info">
+					{/*use activeTab*/}
+					<h2>Job Name</h2>
+					<p>Job information</p>
+				</div>
+			</div>
 		</div>
 	);
 }
